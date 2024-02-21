c4842ad7c6c216ca15786eaf77d7e0f0df5e8dd9
Jarosław Turowski <jaroslawturowski97@gmail.com>
2024-02-21 11:24:50 +0100

    Loop Exercise

---
+++ Tasks/Loop/Program.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Loop;
+    class MyClass
+    {
+    
+    static void Main(string[] args)
+    {
+        int[] temperatureArray = { 18, 20, 10, 35, 16 };
+
+        Console.WriteLine("Najwyższa temperatura przez ostatnie dni to " + FindHighestTemperature(temperatureArray) + " stopni Celsjusza");
+
+        Console.WriteLine("Najniższa temperatura przez ostatnie dni to " + FindLowestTemperature(temperatureArray) + " stopni Celsjusza");
+
+    }
+
+    public static int FindHighestTemperature(int[] temperatures)
+    {
+        int highestTemperature = temperatures[0];
+        for (int i = 1;i < temperatures.Length; i++)
+        {
+            if (temperatures[i] > highestTemperature)
+            {
+                highestTemperature = temperatures[i];
+            }
+        }
+        return highestTemperature;
+    }
+
+    // Znajduje najniższą temperaturę w tablicy temperatur
+    public static int FindLowestTemperature(int[] temperatures)
+    {
+        int lowestTemperatureDegree = temperatures[0];
+        for (int i = 1; i < temperatures.Length; i++)
+        {
+            if (temperatures[i] < lowestTemperatureDegree)
+            {
+                lowestTemperatureDegree = temperatures[i];
+            }
+        }
+        return lowestTemperatureDegree;
+    }
+}
