2917f0cf462e9205d55a31cf9566bd223011ef31
Jarosław Turowski <jaroslawturowski97@gmail.com>
2024-02-21 11:49:22 +0100

    Data Iteration

---
+++ Tasks/Iteration/Program.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Iteration;
+
+class MyClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj liczbę całkowitą");
+        int number = 0;
+        int highestNumber = number;
+        int sum = 0;
+
+        do
+        {   
+            number = int.Parse(Console.ReadLine());
+            sum = sum + number;
+            if (highestNumber < number)
+            {
+                highestNumber = number;
+            }
+        } while (number != 0);
+
+        Console.WriteLine("Suma wszystkich podanych liczb to " + sum);
+        Console.WriteLine($"Największa podana liczba to {highestNumber}");
+    }
+
+}
