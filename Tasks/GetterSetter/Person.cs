1bf822a531ebb035006697a763f2c2ab4bc2e405
Jarosław Turowski <jaroslawturowski97@gmail.com>
2024-02-23 10:16:13 +0100

    Getter Setter Constructor

---
+++ Tasks/GetterSetter/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace GetterSetter
+{
+    internal class Person
+    { 
+        private string name;
+        private DateTime dateOfBirth;
+
+        public DateTime DateOfBirth
+        {
+            get { return dateOfBirth; }
+            set { dateOfBirth = value; }
+        }
+        public Person(string name)
+        {
+            this.name = name;
+        }
+        public Person(string name, DateTime datetime) : this(name)
+        {
+            DateOfBirth = datetime;
+        }
+    }
+}
---
+++ Tasks/GetterSetter/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GetterSetter;
+
+class MyClass
+{
+    public static void Main(string[] args)
+    {
+        Person John = new Person("Jerry",DateTime.Now);
+        John.DateOfBirth = new DateTime(1991, 12, 12);
+        Console.WriteLine(John.DateOfBirth);
+    }    
+}
