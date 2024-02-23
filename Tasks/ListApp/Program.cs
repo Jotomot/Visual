c21ed854337804524ce254e30a7f9c1963c36917
Jarosław Turowski <jaroslawturowski97@gmail.com>
2024-02-23 18:01:24 +0100

    List - methods

---
+++ Tasks/ListApp/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Threading.Tasks;
+
+namespace ListApp;
+
+class MyClass
+{
+    static void Main(string[] args)
+    {
+        TaskManager tasks = new TaskManager();
+
+        tasks.AddTask("Kup 1");
+        tasks.AddTask("Kup 2");
+        List<string> list = tasks.GetTasks();
+
+
+        tasks.RemoveTask("Kup 1");
+        List<string> list2 = tasks.GetTasks();
+        listReader(list2);
+
+    }
+    private static void listReader(List<string> list)
+    {
+       
+        foreach (string item in list)
+        {
+            Console.WriteLine(item);
+        }
+                
+       
+    }
+}
---
+++ Tasks/ListApp/TaskManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListApp
+{
+    internal class TaskManager
+    {
+        private List<string> tasks = new List<string>();
+
+        public void AddTask(string task)
+        {
+            tasks.Add(task);
+        }
+
+        public void RemoveTask(string task)
+        {
+            tasks.Remove(task);
+        }
+
+        public List<string> GetTasks() => tasks;
+
+        public override bool Equals(object? obj)
+        {
+            return obj is TaskManager manager &&
+                   EqualityComparer<List<string>>.Default.Equals(tasks, manager.tasks);
+        }
+    }
+}
