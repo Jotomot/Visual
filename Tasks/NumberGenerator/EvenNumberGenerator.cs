95be793384984fa72464e92fedfcf035a3307ead
Jarosław Turowski <jaroslawturowski97@gmail.com>
2024-02-24 12:46:04 +0100

    NumberGenerator

---
+++ Tasks/NumberGenerator/EvenNumberGenerator.cs
@@ -0,0 +1,24 @@
+﻿namespace NumberGenerator;
+
+class MyClass
+{
+    public static IEnumerable<int> GenerateEvenNumbers()
+    {
+        for (int i = 0; i < 10; i++) 
+        {
+            if ( i % 2 == 0)
+            yield return i;
+        }
+    }
+    public static void Main(string[] args)
+    {
+        var Numbers = GenerateEvenNumbers();
+        foreach (int i in Numbers)
+        {
+            Console.WriteLine("Polecenie wygenerowania kolejnej liczby parzystej ");
+            Console.WriteLine(i);
+        }
+    
+    }
+    
+}
