91ad348ee5e2e77da33420186d7fc314515fa40c
Jarosław Turowski <jaroslawturowski97@gmail.com>
2024-02-24 12:01:45 +0100

    Dictionary and Lambda

---
+++ Tasks/GradeManager/Grades.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeManager
+{
+    internal class Grades
+    {
+        Dictionary<string, List<int>> studentsGrades = new Dictionary<string, List<int>>()
+        {
+            {"Jerry", new List<int>() {} },
+            {"Amy", new List<int>() {} },
+            {"John", new List<int>() {} }
+        };
+
+        public void AddGrade(string studentName, int grade)
+        {
+            if (studentsGrades.ContainsKey(studentName))
+            {
+                studentsGrades[studentName].Add(grade);
+            }
+            else { Console.WriteLine("Podany student nie istnieje"); }
+        
+            
+        }
+
+        public void RemoveGrade(string studentName, int grade)
+        {
+            studentsGrades[studentName].Remove(grade);
+        }
+
+        public double CalculateAverageGrade(string studentName)
+        {
+            return studentsGrades[studentName].Average();
+        }
+
+        public void getListOfStudents()
+        {
+            foreach (var item in studentsGrades)
+            {
+                Console.WriteLine(item.Key);
+            }
+        }
+
+        public void getStudentsGrades(string studentName)
+        {
+            if (studentsGrades.TryGetValue(studentName, out var student))
+            {
+                foreach (var item in student)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+
+        public void getStudentsBestGrade(string studentName)
+        {
+            Console.WriteLine($"Najwyższa ocena studenta {studentName} to: " + studentsGrades[studentName].Max(grade => grade));
+            
+        }
+
+       
+    }
+}
---
+++ Tasks/GradeManager/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace GradeManager;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Grades gradeManager = new Grades();
+
+        gradeManager.getListOfStudents();
+
+        Console.WriteLine("Dodaję oceny 1,2,3,4,5 uczniowi Jerry");
+        gradeManager.AddGrade("Jerry", 1);
+        gradeManager.AddGrade("Jerry", 2);
+        gradeManager.AddGrade("Jerry", 3);
+        gradeManager.AddGrade("Jerrys", 4);
+        gradeManager.AddGrade("Jerry", 5);
+
+        Console.WriteLine("Wyświetlam średnią ocenę Jerry'ego: " + gradeManager.CalculateAverageGrade("Jerry"));
+
+        Console.WriteLine("Usuwam oceny 4 i 5 Jerry'ego ");
+        gradeManager.RemoveGrade("Jerry", 4);
+        gradeManager.RemoveGrade("Jerry", 5);
+        Console.WriteLine("Wyświetlam średnią ocenę Jerry'ego: " + gradeManager.CalculateAverageGrade("Jerry"));
+
+        Console.WriteLine("Wyświetlam oceny Jerrego");
+        gradeManager.getStudentsGrades("Jerry");
+
+        gradeManager.getStudentsBestGrade("Jerry");
+
+    }
+}
