15bd617e3c7f0261fee59003ea70c1a62e0a665b
Jarosław Turowski <jaroslawturowski97@gmail.com>
2024-02-25 16:37:53 +0100

    Celsius to fahrenheit exercise

---
+++ Tasks/Celsius/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Celsius;
+
+Console.WriteLine("Hello, World!");
+TemperatureConverter temperatureConverter = new TemperatureConverter();
+double fahrenheit = 0;
+temperatureConverter.ConvertCelsiusToFahrenheit(35, out fahrenheit);
+
+double celsius = 0;
+temperatureConverter.ConvertFahrenheitToCelsius(0, ref celsius);
+
+Console.WriteLine($" 0 stopni fahrenheit'a to {celsius} stopni celsjusza");
+Console.WriteLine($" 35 stopnie celsjusza to {fahrenheit} stopni fahrenheit'a");
---
+++ Tasks/Celsius/TemperatureConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celsius
+{
+    internal class TemperatureConverter
+    {
+        
+            public void ConvertCelsiusToFahrenheit(double celsius, out double fahrenheit)
+            {
+            fahrenheit =  (celsius * 9 / 5) + 32;
+            }
+
+            public void ConvertFahrenheitToCelsius(double fahrenheit, ref double celsius)
+            {
+             celsius = (fahrenheit - 32) * 5 / 9;
+            }
+        
+    }
+}
