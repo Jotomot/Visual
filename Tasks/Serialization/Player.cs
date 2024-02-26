beeb6e3b3b7af1841207e6d49b0847d84b890af7
Jarosław Turowski <jaroslawturowski97@gmail.com>
2024-02-26 10:47:28 +0100

    Serialize/Desierialize File

---
+++ Tasks/Serialization/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    internal class Player
+    {
+        public string Name { get; set; }
+        public int Level {get;set;}
+
+        public Player(string name, int level)
+        {
+            this.Name = name;
+            this.Level = level;
+        }
+
+       
+    }
+}
---
+++ Tasks/Serialization/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Serialization;
+using Newtonsoft.Json;
+class MyClass
+{
+    public static void Main(string[] args)
+    {
+
+        Player player = new Player("Jerry", 30); 
+            
+
+
+        string serializedPlayer = JsonConvert.SerializeObject(player);
+
+        File.WriteAllText(@"C:\Repozytoria\Visual\Tasks\Serialization\Serialized\player.json", serializedPlayer);
+        string serializedPlayerRead = File.ReadAllText(@"C:\Repozytoria\Visual\Tasks\Serialization\Serialized\player.json");
+
+        Player player2 = JsonConvert.DeserializeObject<Player>(serializedPlayerRead);
+        Console.WriteLine(player2.Name);
+    }
+    
+}
