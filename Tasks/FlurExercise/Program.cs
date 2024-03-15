ed4d574087383a5fa54fd8656968759fd55735e3
Jarosław Turowski <jaroslawturowski97@gmail.com>
2024-03-15 12:18:13 +0100

    Flur exercise

---
+++ Tasks/FlurExercise/Program.cs
@@ -0,0 +1,39 @@
+﻿using Flurl.Http;
+
+namespace FlurExercise;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+       var result = await "https://jsonplaceholder.typicode.com/posts".GetAsync().ReceiveJson<List<Post>>();
+
+        var selectedPost = result.First(p => p.Id == 30);
+        Console.WriteLine(selectedPost.Title);
+        Console.WriteLine(selectedPost.Body);
+
+        selectedPost.Title = "test title";
+        selectedPost.Body = "test body";
+        Console.WriteLine(selectedPost.Title);
+        Console.WriteLine(selectedPost.Body);
+        
+
+
+
+        var postResult = await "https://jsonplaceholder.typicode.com/posts"
+            .WithHeaders(new
+            {
+                header = "value",
+                some_Header = "value2"
+            }, true)
+            .SetQueryParams(new
+            {
+                postID = 1,
+                someValue = "value"
+            })
+            .PostJsonAsync(selectedPost);
+            
+           
+        Console.WriteLine(postResult.StatusCode);
+    }
+}
